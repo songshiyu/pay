c43ac547d1a219c668494bb27083a2ae59e88619
songshiyu <ssy3@meitu.com>
2020-11-10 09:11:19 +0800

    重新启动通用支付项目

--- src/main/java/com/song/pay/PayApplication.java
+++ src/main/java/com/song/pay/PayApplication.java
@@ -1,15 +1,13 @@
 package com.song.pay;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.song.pay.dao")
 public class PayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
 
 }
